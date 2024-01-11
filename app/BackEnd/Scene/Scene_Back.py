5655eb060305660341a788bcf9011b1741fdc525
raphael.attias <raphael.attias@laplateforme.io>
2024-01-11 15:07:07 +0100

    modification

--- app/BackEnd/Scene/Scene_Back.py
+++ app/BackEnd/Scene/Scene_Back.py
@@ -1,13 +1,26 @@
 #!/usr/local/bin/python3
 # -*- coding: utf-8 -*-
-
+import pygame
+import sys
+import os
 class Scene:
     
-    def __init__(self, description, combat=None, ajout_pokedex=None):
+    def __init__(self):
+        pygame.init()
+        self.largeur = 1520
+        self.hauteur = 825
+        self.fenetre = pygame.display.set_mode((self.largeur, self.hauteur))
+        pygame.display.set_caption("pokégame !")
+        dossier_assets = "app/assets/assets_scene"
+        assets_back = "backcombat.png"
+        chemin_back = os.path.join(dossier_assets, assets_back)
+        self.fond = pygame.image.load(chemin_back).convert()
+        
+    def combat(self, description, combat=None, ajout_pokedex=None):
         self.description = description
         self.combat = combat
         self.ajout_pokedex = ajout_pokedex
-        
+          
     def afficher_description(self):
         print(self.description)
         
@@ -31,3 +44,18 @@ class Scene:
             self.ajout_pokedex.sauvegarder_vers_json()
         else:
             print("Pas d'ajout au Pokedex dans cette scène.")
+
+if __name__ == "__main__":
+    scene = Scene()
+    running = True
+    while running:
+        for evenement in pygame.event.get():
+            if evenement.type == pygame.QUIT:
+                running = False
+
+        scene.fenetre.fill((255, 255, 255))
+        scene.fenetre.blit(scene.fond, (0, 0))
+        pygame.display.flip()
+
+    pygame.quit()
+    sys.exit()
