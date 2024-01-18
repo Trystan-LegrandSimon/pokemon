7f5ed60b5126da0a18ade50057895a31d9068b33
Trysta-LegrandSimon <trystan.legrand-simon@laplateforme.io>
2024-01-18 10:14:04 +0100

    update

--- app/app.py
+++ app/app.py
@@ -1,12 +1,10 @@
-import sys
-sys.path.append("app/FrontEnd/Window/")
-from WindowBase import Window
-
 import pygame
 import sys
 import os
 import math
-
+import sys
+sys.path.append("app/FrontEnd/Window/")
+from WindowBase import Window
 
 menu = Window()
 menu.executer()
